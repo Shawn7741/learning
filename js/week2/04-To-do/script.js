98b8e5cd90859fa83e83aa7f4243484a498b1af9
Shawn7741 <shawnpaul480@gmail.com>
2024-06-07 12:42:15 +0300

    end

--- js/week2/04-To-do/script.js
+++ js/week2/04-To-do/script.js
@@ -1,33 +1,25 @@
 const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
-const dltBtn = document.getElementById("delete");
 const taskList = document.getElementById("task-list");
+//Adding task
 addTaskBtn.addEventListener("click", function() {
   const taskText = taskInput.value;
   if (taskText.trim() !== "") {
     const taskItem = document.createElement("li");
    taskItem.innerText = taskText;
-
 taskList.appendChild(taskItem);
     taskInput.value = "";
   }
 });
 
+
+//Removing task
 taskList.addEventListener("click", function(event) {
-  let result = confirm(`Are you sure you want to delete ${taskList.innerText}?`);
+  let answer = confirm(`Do you want to delete ${taskList.innerText}?`);
 
-  if(result === true){
-     if (event.target.tagName.toLowerCase() === "li") {
-    event.target.remove();
+  if (answer === true){
+    if (event.target.tagName.toLowerCase() === "li") {
+      event.target.remove();
+    }
   }
-  }
-  else{
-    alert("You canceled")
-  }
- 
 });
-
-dltBtn.addEventListener("click", function () {
-  const taskText = taskInput.value;
-})
-
