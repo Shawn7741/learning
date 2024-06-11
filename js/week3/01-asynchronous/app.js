f3a22c47f953bd7729adc9487810dd4e3b781ede
Shawn7741 <shawnpaul480@gmail.com>
2024-06-11 11:13:55 +0300

    tue

---
+++ js/week3/01-asynchronous/app.js
@@ -0,0 +1,4 @@
+const name = "Shawn";
+const greetings = `My name is ${name}`;
+
+console.log(greetings);
---
+++ js/week3/01-asynchronous/event.js
@@ -0,0 +1,25 @@
+//call-back
+
+function step1(init){
+    return init + 1;
+}
+
+function step2(init){
+    return init + 2;
+}
+
+
+function step3(init){
+    return init + 3;
+}
+
+function doOperation(){
+    let result = 0;
+    result = step1(result);
+    result = step2(result);
+    result = step3(result);
+
+    console.log(`result: ${result}`);
+}
+
+doOperation();
