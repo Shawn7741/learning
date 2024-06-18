8bb2273191e715f39d8020c7f9307cadefa17160
Shawn7741 <shawnpaul480@gmail.com>
2024-06-18 12:25:14 +0300

    402

---
+++ js/week4/01-javascriptv6/variable.js
@@ -0,0 +1,30 @@
+// let a = 5;
+// let b = 10;
+
+// let sum = (a, b) => a + b;
+
+// console.log(sum(a, b));
+
+// let sum = (a, b) => {
+//     return a + b;
+// };
+
+// console.log(sum(5, 10));
+
+// const sum = (x, y) => x + y;
+
+// console.log (sum(4,5));
+
+
+const person = {
+    name: "Shawn",
+    age: 21,
+    nationality: "Kenyan",
+    hobbies: ["Drawing", "Running"] 
+};
+
+let { name, nationality} = contact;
+
+document.getElementById("demo").innerHTML = name + "is" + nationality; 
+
+
