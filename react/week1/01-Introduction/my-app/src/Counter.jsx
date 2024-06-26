2d758bfe208671faaacf1aedd8a298198c407d5c
Shawn7741 <shawnpaul480@gmail.com>
2024-06-26 13:31:02 +0300

    w14d3

--- react/week1/01-Introduction/my-app/src/Counter.jsx
+++ react/week1/01-Introduction/my-app/src/Counter.jsx
@@ -1,28 +1,80 @@
+// import { useState } from "react";
+
+
+// const Counter = () => {
+
+//     const[count, setCount] = useState(0);
+
+//     const handleIncrement = () => {
+//         setCount(count + 1)
+//     }
+
+//     const handleDecrement = () => {
+//         setCount(count -1)
+//     }
+
+//     return(
+//         <div>
+//             <Counter handleIncrement={handleIncrement} handleDecrement={handleDecrement} count={count}/>
+//             {/* <h1>Counter App</h1>
+//             <br></br>
+//             <h2>{count}</h2>
+//             <br></br>
+//             <button className='bg-sky-400 px-4 py-2 text-white hover:bg-sky-900 sm:py-3' onClick={handleIncrement}>+</button>
+//             <button className='bg-green-400 px-4 py-2 text-white hover:bg-green-900 sm:py-3' onClick={handleDecrement}>-</button> */}
+//         </div>
+//     )
+// }
+
+// // const Counter = () =>{
+// //     const greeting ='Hello react';
+
+// //     return(
+// //         <div>
+// //             <Welcome text={greeting}/>
+// //         </div>
+// //     );
+// // };
+
+// // const Welcome = ({text}) => {
+// //     return <h1>{text}</h1>
+// // };
+
+// // new component
+// const Main = (props) =>{
+
+//     return(
+//         <div className="h-56 grid grid-cols3 gap-4 content-center">
+//             <h1>Counter App</h1>
+//             <br></br>
+//             <h2>{props.count}</h2>
+//             {/*buttons*/}
+//             <button className='bg-sky-400 px-4 py-2 text-white hover:bg-sky-900 sm:py-3' onClick={handleIncrement}>+</button>
+//             <button className='bg-green-400 px-4 py-2 text-white hover:bg-green-900 sm:py-3' onClick={handleDecrement}>-</button>
+//         </div>
+//     )
+// }
+
+// export default Counter
+
+
+
 import { useState } from "react";
 
+function Garage(){
 
-function  Counter(){
-
-    const[count, setCount] = useState(0);
-
-    const handleIncrement = () => {
-        setCount(count + 1)
-    }
-
-    const handleDecrement = () => {
-        setCount(count -1)
-    }
+    const carName = "BMW";
 
     return(
-        <div>
-            <h1>Counter App</h1>
-            <br></br>
-            <h2>{count}</h2>
-            <br></br>
-            <button className='bg-sky-400 px-4 py-2 text-white hover:bg-sky-900 sm:py-3' onClick={handleIncrement}>+</button>
-            <button className='bg-green-400 px-4 py-2 text-white hover:bg-green-900 sm:py-3' onClick={handleDecrement}>-</button>
-        </div>
+        <>
+        <h2>Which care lives in my garage?</h2>
+        <Car brand={carName} />
+        </>
     )
 }
 
-export default Counter
+function Car(props){
+    return <h2>I am a {props.brand}</h2>
+}
+
+export default Garage
