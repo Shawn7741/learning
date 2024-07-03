b1706a1fe645fda63116bc1ea313ac251342b579
Shawn7741 <shawnpaul480@gmail.com>
2024-07-03 12:46:59 +0300

    today

--- react/week1/01-Introduction/my-app/src/main.jsx
+++ react/week1/01-Introduction/my-app/src/main.jsx
@@ -23,28 +23,15 @@
 //   </React.StrictMode>,
 // )
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import RickAndMorty  from './hook'
-// import {BrowserRouter} from 'react-router-dom'//react-router-dom import
-// import './App.css'
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <RickAndMorty/>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App  from './route'
+import RickAndMorty  from './hook'
 import {BrowserRouter} from 'react-router-dom'//react-router-dom import
 import './App.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <RickAndMorty/>
     </BrowserRouter>
   </React.StrictMode>,
 )
