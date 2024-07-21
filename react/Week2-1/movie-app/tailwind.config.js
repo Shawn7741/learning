88a145d680d9e2ee6f2a98533c2119d6a2efa7f6
Shawn7741 <shawnpaul480@gmail.com>
2024-07-21 22:33:23 +0300

    sunday changes

--- react/Week2-1/movie-app/tailwind.config.js
+++ react/Week2-1/movie-app/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+  extend: {},
   },
   plugins: [],
-}
+  }
+  
 
