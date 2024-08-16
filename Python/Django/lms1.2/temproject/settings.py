004b81c664359164aa505fecdbd970f9f4e51d80
Shawn7741 <shawnpaul480@gmail.com>
2024-08-16 13:52:32 +0300

    xyz

--- Python/Django/lms1.2/temproject/settings.py
+++ Python/Django/lms1.2/temproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tempapp',#add your app here
+    'tempapp',
 ]
 
 MIDDLEWARE = [
