578c62ac7eb215ec389f608ab6328607783f7bb5
Shawn7741 <shawnpaul480@gmail.com>
2024-08-16 13:51:33 +0300

    friday

---
+++ Python/Django/example/myapp/views.py
@@ -0,0 +1,19 @@
+# from django.http import HttpResponse
+
+# def welcome(request):
+#     return HttpResponse('Hello world')
+
+from django.shortcuts import render
+from . models import Blog
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog.html', context)
