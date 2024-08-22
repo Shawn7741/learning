5f8c4a2437ed6981e8a02d1c40b0da6335f99c2e
Shawn7741 <shawnpaul480@gmail.com>
2024-08-22 04:32:46 +0300

    early

--- Python/Django/example/myapp/models.py
+++ Python/Django/example/myapp/models.py
@@ -2,40 +2,30 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    
+    email = models.EmailField(unique=True)  # Use EmailField for validation
     def get_absolute_url(self):
-        return reverse("author-detail", args=[str(self.id)])
-    
-    
+        return reverse('author-detail', args=[str(self.id)])
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-    
-
-
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Linking to Django's built-in User model
     content = models.TextField()
-    # image = models.ImageField(upload to='static/' blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     last_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    
-    
     def __str__(self):
         return self.title
-    
     class Meta:
         ordering = ['-date_posted']
-        
     @classmethod
     def published(cls):
         return cls.objects.filter(is_published=True)
-        
-    
-
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.email
--- Python/Django/example/myapp/urls.py
+++ Python/Django/example/myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.index, name='Home'),
     path('about/', views.about, name='About'),
-    path('blog/', views.blog_list, name='Blogs'),
-    
+    path('blog/', views.blog_list, name='Blog'),
+    path('subscribe/', views.subscriber, name='subscribe'),
+    path('add_blog/', views.add_blog, name='add_blog'),
 ]
+
+
+
+
+
